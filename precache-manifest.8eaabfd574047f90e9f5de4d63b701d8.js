9a8b314ebf485fc5c65310e5d0683974899e61ae
Abhay Nath <abhaynath88@gmail.com>
2020-09-20 05:20:43 +0530

    Updates

---
+++ precache-manifest.8eaabfd574047f90e9f5de4d63b701d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c0bb22aeff4d7f62f687159f79ea57",
+    "url": "/github.io/react-app1/index.html"
+  },
+  {
+    "revision": "40bafd53e37e0f14857d",
+    "url": "/github.io/react-app1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b4030eacfee07537388",
+    "url": "/github.io/react-app1/static/js/2.3aea4322.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/react-app1/static/js/2.3aea4322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40bafd53e37e0f14857d",
+    "url": "/github.io/react-app1/static/js/main.7da71bf9.chunk.js"
+  },
+  {
+    "revision": "24d732f1b45872b9aa29",
+    "url": "/github.io/react-app1/static/js/runtime-main.b8cf2be1.js"
+  }
+]);
