a9ec3d2b09cfdb736e1c171c85664d09798eac31
Abhay Nath <abhaynath88@gmail.com>
2020-09-20 05:34:11 +0530

    Updates

---
+++ precache-manifest.83f222ec7b9f083cc86125038d05e919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cdbb82ead689902dfe7ec370eba54a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e5ac17ab51b2d44bd30",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b4030eacfee07537388",
+    "url": "/static/js/2.3aea4322.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3aea4322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e5ac17ab51b2d44bd30",
+    "url": "/static/js/main.3f7392a0.chunk.js"
+  },
+  {
+    "revision": "a3d2bf21716328fc74a3",
+    "url": "/static/js/runtime-main.d8499093.js"
+  }
+]);
